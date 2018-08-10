0b4645878168cf09b4fdb4676a62d9d222afea3e
薛兴运 <loveklmn@126.com>
2018-08-10 11:07:50 +0000

    Init basic Interface based on Mpvue template Ref #5.

---
+++ WxFrontend/src/pages/login-in/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button class="login" @click="login"> login </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+
+  methods: {
+    login () {
+      // implement verification
+      // if passed
+      wx.switchTab({
+        url: '/pages/index/main'
+      })
+    }
+  },
+
+  created () {}
+}
+</script>
+
+<style scoped>
+.login {
+  margin-top : 100%
+}
+</style>
