66700d14bc77f496d1a9f1a9873ebfcadee0e8a4
Victarry <nkulzh16@gmail.com>
2018-08-12 20:46:03 +0000

    Register model for admin management. Ref #10

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.conf.urls import url, include
+from django.urls import path
+from django.contrib import admin
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
+    path('admin/', admin.site.urls)
 ]
