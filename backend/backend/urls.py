1efbead5b47db125dedbd34e7ea48663f6775128
Victarry <nkulzh16@gmail.com>
2018-08-12 04:36:10 +0000

    Configure git hook with pylint for bankend project. Ref #9

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,13 +17,13 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from backend.vron import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+ROUTER = routers.DefaultRouter()
+ROUTER.register(r'users', views.UserViewSet)
+ROUTER.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^', include(ROUTER.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
