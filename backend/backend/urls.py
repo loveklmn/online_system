c788f0322a0b90a0cf1e023bbee8b0c842710cc7
Victarry <nkulzh16@gmail.com>
2018-08-12 19:16:25 +0000

    Format django app. Ref #10

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,17 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from rest_framework import routers
-from backend.vron import views
-
-ROUTER = routers.DefaultRouter()
-ROUTER.register(r'users', views.UserViewSet)
-ROUTER.register(r'groups', views.GroupViewSet)
+# from django.conf.urls import url, include
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(ROUTER.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
