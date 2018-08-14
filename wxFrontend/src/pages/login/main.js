dd39f4476bfbf9486e2f649c61a84bde1cff2580
Victarry <nkulzh16@gmail.com>
2018-08-14 18:17:37 +0000

    Implement login logic in login page. Ref #23

--- wxFrontend/src/pages/login/main.js
+++ wxFrontend/src/pages/login/main.js
@@ -3,3 +3,16 @@ import App from './index'
 
 const app = new Vue(App)
 app.$mount()
+
+export default {
+  config: {
+    usingComponents: {
+      'i-cell-group': '../../static/iview/cell-group/index',
+      'i-cell': '../../static/iview/cell/index',
+      'i-panel': '../../static/iview/panel/index',
+      'i-button': '../../static/iview/button/index',
+      'i-card': '../../static/iview/card/index',
+      'i-icon': '../../static/iview/icon/index'
+    }
+  }
+}
