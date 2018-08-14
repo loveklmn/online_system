c820e2ae494e2e88304ce460cb70ebfe19c1af1f
Victarry <nkulzh16@gmail.com>
2018-08-14 17:50:03 +0000

    Wrap request for global using. Ref #23

---
+++ wxFrontend/src/utils/request.js
@@ -0,0 +1,46 @@
+class Request {
+  constructor (parms) {
+    this.withBaseURL = parms.withBaseURL
+    this.baseURL = parms.baseURL
+    this.header = parms.header
+  }
+  get (url, data) {
+    return this.request('GET', url, data)
+  }
+  post (url, data) {
+    return this.request('POST', url, data)
+  }
+  put (url, data) {
+    return this.request('PUT', url, data)
+  }
+  request (method, url, data) {
+    const vm = this
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: vm.withBaseURL ? vm.baseURL + url : url,
+        data,
+        method,
+        header: vm.header,
+        success (res) {
+          resolve(res)
+        },
+        fail () {
+          reject(new Error({
+            msg: '请求失败',
+            url: vm.withBaseURL ? vm.baseURL + url : url,
+            method,
+            data
+          }))
+        }
+      })
+    })
+  }
+}
+
+export default new Request({
+  baseURL: 'http://192.168.55.33:8000/vron',
+  withBaseURL: true,
+  header: {
+    'content-type': 'application/x-www-form-urlencoded'
+  }
+})
