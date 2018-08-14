cd407aa6cd7645f7e7c0fe4e868e144bf1da62a3
JinPengxiang <neexgzyx@163.com>
2018-08-14 13:13:24 +0000

    Add LoginTestCase in vron/tests.py. Ref #24

--- backend/vron/tests.py
+++ backend/vron/tests.py
@@ -1,3 +1,43 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.test import Client
+from .models import Student
+from django.contrib.auth.models import User
+import json
+from urllib.parse import urlencode
 
-# Create your tests here.
+class LoginTestCase(TestCase):
+    
+    client = Client()
+    url = '/vron/login/'
+    type = 'application/x-www-form-urlencoded'
+
+    def setUp(self):
+        user1 = User.objects.create_user(username = 'zhangsan', password = '][po][po')
+        student1 = Student.objects.create(user = user1, level = 2, score = 0)
+        user2 = User.objects.create_user(username = 'lisi', password = '][po][po')
+
+    def post_tool(self, input, output):
+
+        data = urlencode(input)
+        response = self.client.post(self.url, data = data, content_type = self.type)
+        self.assertEqual(json.loads(response.content),output)
+
+    def test(self):
+        response = self.client.get(self.url)
+        self.assertEqual(json.loads(response.content), {'msg': 'please use POST method to login'})
+
+        input = {'username':'zhangsan', 'password':'][po][po'}
+        output = {'userid': 1, 'level': 2}
+        self.post_tool(input,output)
+
+        input = {'username':'zhangsan', 'password':''}
+        output = {'msg': 'username or password error.'}
+        self.post_tool(input,output)
+
+        input = {'username':'', 'password':'asdf'}
+        output = {'msg': 'username or password error.'}
+        self.post_tool(input,output)
+
+        input = {'username':'lisi', 'password':'][po][po'}
+        output = {'msg': 'your account have not connect to a student'}
+        self.post_tool(input,output)
