304aa4d0e6ff34c52fe80180eddfebfac20b169c
JinPengxiang <neexgzyx@163.com>
2018-08-15 09:38:10 +0000

    Add display of matching game. Ref #30

---
+++ wxFrontend/src/components/matchingGamePic.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="pic_block">
+    <img
+      class="pic"
+      :src="src"
+      />
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      selected: false
+    }
+  },
+  props: ['src'],
+  methods: {
+  }
+}
+</script>
+
+<style>
+.pic {
+  display: block;
+  width: 200rpx;
+  height: 200rpx;
+  margin: auto;
+  padding: auto;
+}
+.pic_block {
+  width: 100%;
+  margin: auto;
+  padding: auto;
+}
+</style>
