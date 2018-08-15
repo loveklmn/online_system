0939ad021803069387b89354a0606efa67e3a330
mrwen33@live.com <mrwen33@live.com>
2018-08-15 01:29:24 +0000

    Remove login function and add token authenticate. Ref #25

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'vron'
 ]
 
@@ -45,8 +46,11 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+        'rest_framework.permissions.IsAuthenticated'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
 
 MIDDLEWARE = [
--- backend/vron/views.py
+++ backend/vron/views.py
@@ -1,47 +1,20 @@
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http import JsonResponse
-from django.contrib.auth.models import User
-from django.contrib import auth
 from vron.serializers import BookSerializer
 from vron.models import Student, Book, Progress, Word, Page, Sentence, Moment
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view
 # Create your views here.
 
-def login_require(func):
-    def wrapper(*args, **kw):
-        request = args[0]
-        if request.user.is_authenticated:
-            return func(*args, **kw)
-        else:
-            return JsonResponse({'msg': 'Please login first.'})
-    return wrapper
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    sender = sender
+    kwargs = kwargs
+    if created:
+        Token.objects.create(user=instance)
 
 @csrf_exempt
-def login(request):
-    """
-    login function
-    """
-
-    #if request.user.is_authenticated:
-     #   return JsonResponse({'msg': 'you have been logged in.'})
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user =  auth.authenticate(username=username, password=password)
-        if user is not None and user.is_active:
-            auth.login(request, user)
-            userobj = User.objects.get_by_natural_key(username)
-            if not Student.objects.filter(user=userobj):
-                return JsonResponse({'msg': 'your account have not connect to a student'})
-            student = Student.objects.get(user=userobj)
-            return JsonResponse({'userid': userobj.id, 'level': student.level})
-        else:
-            return JsonResponse({'msg': 'username or password error.'})
-    else:
-        return JsonResponse({'msg': 'please use POST method to login'})
-
-@csrf_exempt
-@login_require
+@api_view(['GET','POST'])
 def book_list(request):
     """
     List all books of one level, or create a new book of this level
@@ -86,7 +59,7 @@ def book_list(request):
             return JsonResponse(serializer.errors, status=400)
 
 @csrf_exempt
-@login_require
+@api_view(['GET','POST'])
 def book_guidance(request, book_id):
     '''
     List guidance of a book, or add a guidance of a book.
@@ -115,7 +88,7 @@ def book_guidance(request, book_id):
         return JsonResponse({'msg': 'Please use GET method'})
 
 @csrf_exempt
-@login_require
+@api_view(['GET','POST'])
 def book_ebook(request, book_id):
     if request.method == 'GET':
         book_data = Book.objects.filter(id=book_id)
@@ -147,9 +120,8 @@ def book_ebook(request, book_id):
     else:
         return JsonResponse({'msg': 'Please use GET method'})
 
-<<<<<<< HEAD
 @csrf_exempt
-@login_require
+@api_view(['GET','POST'])
 def community_group(request, level):
     if request.method == 'GET':
         community_info = []
@@ -168,5 +140,3 @@ def community_group(request, level):
         return JsonResponse(community_info, safe=False)
     else:
         return JsonResponse({'msg': 'Please use GET method.'})
-=======
->>>>>>> Add backend api:ebook. Ref #21
