e7cf381a26d2b18cb25448d1ff7661a7bab2a30c
JinPengxiang <neexgzyx@163.com>
2018-08-18 16:10:02 +0000

    Add props to moment component. Ref #52

--- wxFrontend/src/components/moment.vue
+++ wxFrontend/src/components/moment.vue
@@ -37,28 +37,9 @@
 export default {
   data () {
     return {
-      x: {
-        author: {
-          username: '弗恩英语客服',
-          avatar: 'https://unsplash.it/400/800/?random'
-        },
-        book: {
-          cover: 'http://xxxxxxx/x.jpg',
-          title: 'mpvue从入坑到弃坑'
-        },
-        created_time: '13个月前',
-        content: '欢迎大家来弗恩英语学习',
-        attachments: {
-          image: [
-            'https://unsplash.it/400/800/?random',
-            'https://unsplash.it/400/800/?random',
-            'https://unsplash.it/400/800/?random',
-            'https://unsplash.it/400/800/?random'
-          ]
-        }
-      }
     }
   },
+  props: ['x'],
   computed: {
     imgClass () {
       let size = this.x.attachments.image.length
