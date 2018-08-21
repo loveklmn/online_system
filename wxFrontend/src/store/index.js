cddda270a467185a07be72a3c6453482298453ec
Victarry <nkulzh16@gmail.com>
2018-08-21 06:28:59 +0000

    Use local storage to store token. Ref #75

--- wxFrontend/src/store/index.js
+++ wxFrontend/src/store/index.js
@@ -9,7 +9,14 @@ const store = new Vuex.Store({
   },
   mutations: {
     setToken (state, token) {
+      wx.setStorageSync('token', token)
       state.token = token
+    }
+  },
+  getters: {
+    token: (state) => {
+      let token = state.token || wx.getStorageSync('token')
+      return token
     }
   }
 })
