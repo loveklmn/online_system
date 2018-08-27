f54908ffaef85f1a543bc5d052b0305a1c212408
JinPengxiang <neexgzyx@163.com>
2018-08-27 02:45:33 +0000

    Implement game navigator. Ref #136

---
+++ adminFrontend/src/view/import-game/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Tabs type="card">
+      <TabPane label="连线游戏">标签一的内容</TabPane>
+      <TabPane label="拼图游戏">标签二的内容</TabPane>
+      <TabPane label="单词识别">标签三的内容</TabPane>
+      <TabPane label="选词填空">标签三的内容</TabPane>
+  </Tabs>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
