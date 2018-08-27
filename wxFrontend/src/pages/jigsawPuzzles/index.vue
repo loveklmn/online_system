4a2da8e5019ce61addef84649783959b6f7ed8ba
薛兴运Xingyun Xue <loveklmn@126.com>
2018-08-27 20:44:47 +0800

    Fix puzzles. Ref #126

--- wxFrontend/src/pages/jigsawPuzzles/index.vue
+++ wxFrontend/src/pages/jigsawPuzzles/index.vue
@@ -1,28 +1,46 @@
 <template>
   <div>
-      <div class="container">
+    <image class="main-bg" src="../../static/images/main-bg.png" />
+    <div class="container">
         <div class="pintu-wrap">
             <div class="pintu-line">
-                <div :style="icon1" @click="touch1" data-index=3 data-space=1></div>
-                <div :style="icon2" @click="touch2" data-index=5 data-space=2></div>
-                <div :style="icon3" @click="touch3" data-index=6 data-space=3></div>
+                <div class="pintu-item-wrap">
+                    <image class="pintu-item" type="primary" :src="cards.card1.src" :hidden="cards.card1.hidden" @click="card1" />
+                </div>
+                <div class="pintu-item-wrap">
+                    <image class="pintu-item" type="primary" :src="cards.card2.src" :hidden="cards.card2.hidden" @click="card2" />
+                </div>
+                <div class="pintu-item-wrap">
+                    <image class="pintu-item" type="primary" :src="cards.card3.src" :hidden="cards.card3.hidden" @click="card3" />
+                </div>
             </div>
             <div class="pintu-line">
-                <div :style="icon4" @click="touch4" data-index=8 data-space=4></div>
-                <div :style="icon5" @click="touch5" data-index=1 data-space=5></div>
-                <div :style="icon6" @click="touch6" data-index=4 data-space=6></div>
+                <div class="pintu-item-wrap">
+                    <image class="pintu-item" type="primary" :src="cards.card4.src" :hidden="cards.card4.hidden" @click="card4" />
+                </div>
+                <div class="pintu-item-wrap">
+                    <image class="pintu-item" type="primary" :src="cards.card5.src" :hidden="cards.card5.hidden" @click="card5" />
+                </div>
+                <div class="pintu-item-wrap">
+                    <image class="pintu-item" type="primary" :src="cards.card6.src" :hidden="cards.card6.hidden" @click="card6" />
+                </div>
             </div>
             <div class="pintu-line">
-                    <div :style="icon7" @click="touch7" data-index=2 data-space=7></div>
-                    <div :style="icon8" @click="touch8" data-index=7 data-space=8></div>
-                    <div :style="icon9" @click="touch9" data-index=9 data-space=9></div>
+                <div class="pintu-item-wrap">
+                    <image class="pintu-item" type="primary" :src="cards.card7.src" :hidden="cards.card7.hidden" @click="card7" />
+                </div>
+                <div class="pintu-item-wrap">
+                    <image class="pintu-item" type="primary" :src="cards.card8.src" :hidden="cards.card8.hidden" @click="card8" />
+                </div>
+                <div class="pintu-item-wrap">
+                    <image class="pintu-item" type="primary" :src="cards.card9.src" :hidden="cards.card9.hidden" @click="card9" />
+                </div>
             </div>
         </div>
         <div class="btn-wrap">
             <button type="warn" @click="initGame">重新开始</button>
         </div>
     </div>
-
 </div>
 </template>
 
@@ -30,141 +48,127 @@
 export default {
   data () {
     return {
-      picture: 'https://i.loli.net/2018/08/27/5b82f5e0be22a.png',
-      note: -1,
-      space: -1,
-      arr: ['3', '5', '6', '8', '1', '4', '2', '7', '9'],
-      icon: ''
-    }
-  },
-  computed: {
-    icon1: function () {
-      return 'top:0px;left:0px;background:url(' + this.picture + ') no-repeat -500rpx 0px/750rpx 1206rpx;width:250rpx;height:402rpx;'
-    },
-    icon2: function () {
-      return 'top:0px;left:250rpx;background:url(' + this.picture + ') no-repeat -250rpx -402rpx/750rpx 1206rpx;width:250rpx;height:402rpx;'
-    },
-    icon3: function () {
-      return 'top:0px;left:500rpx;background:url(' + this.picture + ') no-repeat -500rpx -402rpx/750rpx 1206rpx;width:250rpx;height:402rpx;'
-    },
-    icon4: function () {
-      return 'top:402rpx;left:0px;background:url(' + this.picture + ') no-repeat -250rpx -804rpx/750rpx 1206rpx;width:250rpx;height:402rpx;'
-    },
-    icon5: function () {
-      return 'top:402rpx;left:250rpx;background:url(' + this.picture + ') no-repeat 0px 0px/750rpx 1206rpx;width:250rpx;height:402rpx;'
-    },
-    icon6: function () {
-      return 'top:402rpx;left:500rpx;background:url(' + this.picture + ') no-repeat 0px -402rpx/750rpx 1206rpx;width:250rpx;height:402rpx;'
-    },
-    icon7: function () {
-      return 'top:804rpx;left:0px;background:url(' + this.picture + ') no-repeat -250rpx 0px/750rpx 1206rpx;width:250rpx;height:402rpx;'
-    },
-    icon8: function () {
-      return 'top:804rpx;left:250rpx;background:url(' + this.picture + ') no-repeat 0px -804rpx/750rpx 1206rpx;width:250rpx;height:402rpx;'
-    },
-    icon9: function () {
-      return 'top:804rpx;left:500rpx;background:url(' + this.picture + ') no-repeat -500rpx -804rpx/750rpx 1206rpx;width:250rpx;height:402rpx;'
+      word: 'It is a lucky cat.',
+      cards: {
+        card1: {num: 3, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744aef8183b.jpg'},
+        card2: {num: 7, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744ae8bb097.jpg'},
+        card3: {num: 5, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744ae679f2c.jpg'},
+        card4: {num: '', hidden: true, src: 'https://i.loli.net/2018/08/15/5b744ae453d4f.jpg'},
+        card5: {num: 4, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744ae274208.jpg'},
+        card6: {num: 6, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744add508b0.jpg'},
+        card7: {num: 2, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744ad901dab.jpg'},
+        card8: {num: 8, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744ad4b1fb6.jpg'},
+        card9: {num: 1, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744ad026a65.png'}
+      }
     }
   },
   methods: {
-    touch1 (e) {
-      if (this.space === -1 || this.space === e.currentTarget.dataset.space) {
-        this.space = e.currentTarget.dataset.space
-        console.log('condition 1, space = ' + this.space)
-        return 0
-      } else {
-        console.log('condition 2, you touched ' + this.space + '. you touch 3 now')
-        this.space = -1
+    card1 () {
+      this.moveCard('1', '2')
+      this.moveCard('1', '4')
+    },
+    card2 () {
+      this.moveCard('2', '1')
+      this.moveCard('2', '3')
+      this.moveCard('2', '5')
+    },
+    card3 () {
+      this.moveCard('3', '2')
+      this.moveCard('3', '6')
+    },
+    card4 () {
+      this.moveCard('4', '1')
+      this.moveCard('4', '5')
+      this.moveCard('4', '7')
+    },
+    card5 () {
+      this.moveCard('5', '2')
+      this.moveCard('5', '4')
+      this.moveCard('5', '6')
+      this.moveCard('5', '8')
+    },
+    card6 () {
+      this.moveCard('6', '3')
+      this.moveCard('6', '5')
+      this.moveCard('6', '9')
+    },
+    card7 () {
+      this.moveCard('7', '4')
+      this.moveCard('7', '8')
+    },
+    card8 () {
+      this.moveCard('8', '5')
+      this.moveCard('8', '7')
+      this.moveCard('8', '9')
+    },
+    card9 () {
+      this.moveCard('9', '6')
+      this.moveCard('9', '8')
+    },
+    initGame () {
+      this.cards = {
+        card1: {num: 3, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744aef8183b.jpg'},
+        card2: {num: 7, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744ae8bb097.jpg'},
+        card3: {num: 5, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744ae679f2c.jpg'},
+        card4: {num: '', hidden: true, src: 'https://i.loli.net/2018/08/15/5b744ae453d4f.jpg'},
+        card5: {num: 4, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744ae274208.jpg'},
+        card6: {num: 6, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744add508b0.jpg'},
+        card7: {num: 2, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744ad901dab.jpg'},
+        card8: {num: 8, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744ad4b1fb6.jpg'},
+        card9: {num: 1, hidden: false, src: 'https://i.loli.net/2018/08/15/5b744ad026a65.png'}
       }
     },
-    touch2 (e) {
-      if (this.space === -1 || this.space === e.currentTarget.dataset.space) {
-        this.space = e.currentTarget.dataset.space
-        console.log('condition 1, space = ' + this.space)
-        return 0
-      } else {
-        console.log('condition 2, you touched ' + this.space + '. you touch 3 now')
-        this.space = -1
-      }
-    },
-    touch3 (e) {
-      if (this.space === -1 || this.space === e.currentTarget.dataset.space) {
-        this.space = e.currentTarget.dataset.space
-        console.log('condition 1, space = ' + this.space)
-        return 0
-      } else {
-        console.log('condition 2, you touched ' + this.space + '. you touch 3 now')
-        this.space = -1
-      }
-    },
-    touch4 (e) {
-      if (this.space === -1 || this.space === e.currentTarget.dataset.space) {
-        this.space = e.currentTarget.dataset.space
-        console.log('condition 1, space = ' + this.space)
-        return 0
-      } else {
-        console.log('condition 2, you touched ' + this.space + '. you touch 4 now')
-        this.space = -1
-      }
-    },
-    touch5 (e) {
-      if (this.space === -1 || this.space === e.currentTarget.dataset.space) {
-        this.space = e.currentTarget.dataset.space
-        console.log('condition 1, space = ' + this.space)
-        return 0
-      } else {
-        console.log('condition 2, you touched ' + this.space + '. you touch 5 now')
-        this.space = -1
-      }
-    },
-    touch6 (e) {
-      if (this.space === -1 || this.space === e.currentTarget.dataset.space) {
-        this.space = e.currentTarget.dataset.space
-        console.log('condition 1, space = ' + this.space)
-        return 0
-      } else {
-        console.log('condition 2, you touched ' + this.space + '. you touch 6 now')
-        this.space = -1
-      }
-    },
-    touch7 (e) {
-      if (this.space === -1 || this.space === e.currentTarget.dataset.space) {
-        this.space = e.currentTarget.dataset.space
-        console.log('condition 1, space = ' + this.space)
-        return 0
-      } else {
-        console.log('condition 2, you touched ' + this.space + '. you touch 7 now')
-        this.space = -1
-      }
-    },
-    touch8 (e) {
-      if (this.space === -1 || this.space === e.currentTarget.dataset.space) {
-        this.space = e.currentTarget.dataset.space
-        console.log('condition 1, space = ' + this.space)
-        return 0
-      } else {
-        console.log('condition 2, you touched ' + this.space + '. you touch 8 now')
-        this.space = -1
-      }
-    },
-    touch9 (e) {
-      if (this.space === -1 || this.space === e.currentTarget.dataset.space) {
-        this.space = e.currentTarget.dataset.space
-        console.log('condition 1, space = ' + this.space)
-        return 0
-      } else {
-        console.log('condition 2, you touched ' + this.space + '. you touch 9 now')
-        this.space = -1
+    moveCard (n1, n2) {
+      let cards = this.cards
+      let c1 = cards['card' + n1]
+      let c2 = cards['card' + n2]
+      if (c1.num && !c2.num) {
+        let num1 = c1.num
+        let hidden1 = c1.hidden
+        let src1 = c1.src
+        let num2 = c2.num
+        let hidden2 = c2.hidden
+        let src2 = c2.src
+        cards['card' + n1].num = num2
+        cards['card' + n1].hidden = hidden2
+        cards['card' + n1].src = src2
+        cards['card' + n2].num = num1
+        cards['card' + n2].hidden = hidden1
+        cards['card' + n2].src = src1
+        let isGameOver = true
+        for (let i = 1; i < 9; i++) {
+          if (cards['card' + i].num !== i) {
+            isGameOver = false
+            break
+          }
+        }
+        if (isGameOver) {
+          cards.card9.num = 9
+          cards.card9.hidden = false
+        }
+        this.cards = cards
+
+        if (isGameOver) {
+          wx.showModal({
+            title: 'Oooops',
+            content: this.word,
+            showCancel: false
+          })
+        }
       }
     }
   }
 }
 </script>
+
 <style>
+input{
+  padding: 20rpx
+}
 .container {
   height: 100%;
   display: flex;
-  /* padding-top: 200rpx; */
+  padding-top: 200rpx;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
@@ -173,7 +177,7 @@ export default {
 }
 
 .btn-wrap {
-  width: 100%;
+  width: 80%;
   padding: 20px 0;
   text-align: center;
   margin: auto;
@@ -182,7 +186,7 @@ export default {
 .pintu-wrap {
   width: 100%;
   align-items: center;
-  /* padding-left: 60rpx; */
+  padding-left: 60rpx;
 }
 
 .pintu-line {
